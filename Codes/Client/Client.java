bc0b7552ca3a797b392becd9b4a11312c00d77e6
Arnabbndc <1905065@ugrad.cse.buet.ac.bd>
2023-06-16 02:42:22 +0600

    File uploading done

--- Codes/Client/Client.java
+++ Codes/Client/Client.java
@@ -1,8 +1,6 @@
 package Client;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
@@ -13,13 +11,63 @@ public class Client {
         System.out.println("Local port: " + socket.getLocalPort());
 
         // buffers
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());
 
         //
         while(true) {
-            String msg = (String) in.readObject();
-            System.out.println(msg);
+//            String msg = (String) in.readObject();
+//            System.out.println(msg);
+            //sending file
+            File file = new File("Codes/Client/abcd.txt");
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+//            long fileLength = file.length();
+
+            out.writeUTF("fileName "+ "abcd.txt"+" "+file.length());
+            System.out.println("fileName "+  "abcd.txt"+" "+file.length());
+            out.flush();
+
+            // break file into chunks
+            int bytes = 0;
+            byte[] buffer = new byte[512];
+            int CHUNK = 0;
+            while ((bytes=fileInputStream.read(buffer))!=-1){
+
+//            if(CHUNK % 10000 == 0) System.out.println("Chunk #"+CHUNK);
+                CHUNK++;
+
+                out.write(buffer,0,bytes);
+                out.flush();
+
+//                try {
+//                    // ACK
+//                    String msg = dataInputStreamFile.readUTF();
+//                    if(!msg.equals("ACK"))
+//                    {
+//                        System.out.println("Did not receive ACK...");
+//                        break;
+//                    }
+//
+//                }catch (SocketTimeoutException socketTimeoutException){
+//                    System.out.println("TIMEOUT");
+//                    out.writeUTF("TIMEOUT "+fileType+" "+fileName);
+//                    out.flush();
+//                    fileInputStream.close();
+//                    return;
+//                }
+            }
+            fileInputStream.close();
+
+            // send confirmation
+            out.writeUTF("ACK");
+            out.flush();
+
+            String msg = in.readUTF();
+            if(msg.equals("ACK")) {System.out.println("File Upload Completed");
+                return;}
+            else System.out.println("File Upload Failed");
+
         }
     }
 }
